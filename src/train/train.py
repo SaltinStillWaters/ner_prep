72dce17e76310e249192d63fa3be74b1be27d463
SaltinStillWaters <SaltinStillWaters@gmail.com>
2025-06-29 11:02:54 +0800

    latest coorections

--- src/train/train.py
+++ src/train/train.py
@@ -47,7 +47,7 @@ def main(aug_dataset, dataset_name, eval_to_use):
         
         per_device_eval_batch_size=eval_to_use,
         
-        num_train_epochs=1,
+        num_train_epochs=10,
         learning_rate=3e-5,
         per_device_train_batch_size=8,
         weight_decay=0.421288,
